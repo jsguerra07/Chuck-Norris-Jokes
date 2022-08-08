037d73c9aed3da45f50d43b739d3918f032ac8a6
Sebastian Guerra <js.guerra07@hotmail.com>
2022-08-07 20:25:08 -0500

    commit 2

--- client/src/supabase-api/supabase.js
+++ client/src/supabase-api/supabase.js
@@ -1,13 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-/* const {
-  REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY
-} = process.env;
- */
-/* export const supabase = createClient(
-  `${REACT_APP_SUPABASE_URL}`,
-  `${REACT_APP_SUPABASE_KEY}`
-);
- */
+
 export const supabase = createClient(
   "https://lbjfmccixgvynavxiniq.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiamZtY2NpeGd2eW5hdnhpbmlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2MzQ4MzYsImV4cCI6MTk3NTIxMDgzNn0.FmnCGavLPlLv_3r2GDZC5-9qwtdROIdtcxQEFvoAZzI"
